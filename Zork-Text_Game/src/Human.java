aed59cd099bfb666f11ce1e47d9813bcfc0d6a44
Unknown <elonas457@gmail.com>
2018-07-23 02:55:37 -0400

    Mejorando Humano
    Mejorando el HUD del Humano:
    muestra vida, peso, niveles e inventario

--- Zork-Text_Game/src/Human.java
+++ Zork-Text_Game/src/Human.java
@@ -16,54 +16,135 @@ public class Human {
 	ArrayList<Gun> Gun_list= new ArrayList<Gun>();
 	ArrayList<Armor> Armor_list= new ArrayList<Armor>();
 	
-	public void showGun(Gun G) {
+	public void showAllGun() {
+		for(int i=0;i<this.Gun_list.size();i++) {
+			
 		System.out.println("----------------------------");
-		System.out.println("Tines una: " + G.getName());
+		System.out.println("  " + this.Gun_list.get(i).getName());
 		System.out.println("");
 		System.out.println("Daño:");
-		System.out.println("  Fisico: " + G.getFisicDmg());
-		System.out.println("  Energia: " + G.getEnergyDmg());
-		System.out.println("  Radiacion: " + G.getRadDmg());
+		System.out.println("  Fisico: " + this.Gun_list.get(i).getFisicDmg());
+		System.out.println("  Energia: " + this.Gun_list.get(i).getEnergyDmg());
+		System.out.println("  Radiacion: " + this.Gun_list.get(i).getRadDmg());
 		System.out.println("");
-		if(G.getFisicAmmoCost()!=0) {
-			if(G.getFisicAmmoCost()==1) {
-		System.out.println("Esta arma utiliza: " + G.getFisicAmmoCost() + " bala");
+		if(this.Gun_list.get(i).getFisicAmmoCost()!=0) {
+			if(this.Gun_list.get(i).getFisicAmmoCost()==1) {
+		System.out.println("Esta arma utiliza: " + this.Gun_list.get(i).getFisicAmmoCost() + " bala");
 			}
-			else if(G.getFisicAmmoCost()>1) {
-				System.out.println("Esta arma utiliza: " + G.getFisicAmmoCost() + " balas");
+			else if(this.Gun_list.get(i).getFisicAmmoCost()>1) {
+				System.out.println("Esta arma utiliza: " + this.Gun_list.get(i).getFisicAmmoCost() + " balas");
 			}
 		}
-		else if(G.getEnergyAmmoCost()!=0){
-			if(G.getEnergyAmmoCost()==1) {
-				System.out.println("Esta arma utiliza: " + G.getEnergyAmmoCost() + " celda de energia");
+		else if(this.Gun_list.get(i).getEnergyAmmoCost()!=0){
+			if(this.Gun_list.get(i).getEnergyAmmoCost()==1) {
+				System.out.println("Esta arma utiliza: " + this.Gun_list.get(i).getEnergyAmmoCost() + " celda de energia");
 					}
-					else if(G.getEnergyAmmoCost()>1) {
-						System.out.println("Esta arma utiliza: " + G.getEnergyAmmoCost() + " celdas de energia");
+					else if(this.Gun_list.get(i).getEnergyAmmoCost()>1) {
+						System.out.println("Esta arma utiliza: " + this.Gun_list.get(i).getEnergyAmmoCost() + " celdas de energia");
 					}
 		}
-		else if(G.getRadAmmoCost()!=0){
-			if(G.getRadAmmoCost()==1) {
-				System.out.println("Esta arma utiliza: " + G.getRadAmmoCost() + " Cartuchos Gamma");
+		else if(this.Gun_list.get(i).getRadAmmoCost()!=0){
+			if(this.Gun_list.get(i).getRadAmmoCost()==1) {
+				System.out.println("Esta arma utiliza: " + this.Gun_list.get(i).getRadAmmoCost() + " Cartuchos Gamma");
 					}
-					else if(G.getRadAmmoCost()>1) {
-						System.out.println("Esta arma utiliza: " + G.getRadAmmoCost() + " Cartuchos Gamma");
+					else if(this.Gun_list.get(i).getRadAmmoCost()>1) {
+						System.out.println("Esta arma utiliza: " + this.Gun_list.get(i).getRadAmmoCost() + " Cartuchos Gamma");
 					}
 		}
 		System.out.println("");
-		System.out.println("Peso: " + G.getWeigh());
+		System.out.println("Peso: " + this.Gun_list.get(i).getWeigh());
+		System.out.println("");
+		}
+	}
+	
+	public void showGunNames() {
+
+		for(int i=0;i<this.Gun_list.size();i++) {
+			System.out.println(i+1+"-"+this.Gun_list.get(i).getName());
+		}
+	}
+	
+	public void showGun(int option) {
+		System.out.println("----------------------------");
+		System.out.println("  " + this.Gun_list.get(option-1).getName());
+		System.out.println("");
+		System.out.println("Daño:");
+		System.out.println("  Fisico: " + this.Gun_list.get(option-1).getFisicDmg());
+		System.out.println("  Energia: " + this.Gun_list.get(option-1).getEnergyDmg());
+		System.out.println("  Radiacion: " + this.Gun_list.get(option-1).getRadDmg());
+		System.out.println("");
+		if(this.Gun_list.get(option-1).getFisicAmmoCost()!=0) {
+			if(this.Gun_list.get(option-1).getFisicAmmoCost()==1) {
+		System.out.println("Esta arma utiliza: " + this.Gun_list.get(option-1).getFisicAmmoCost() + " bala");
+			}
+			else if(this.Gun_list.get(option-1).getFisicAmmoCost()>1) {
+				System.out.println("Esta arma utiliza: " + this.Gun_list.get(option-1).getFisicAmmoCost() + " balas");
+			}
+		}
+		else if(this.Gun_list.get(option-1).getEnergyAmmoCost()!=0){
+			if(this.Gun_list.get(option-1).getEnergyAmmoCost()==1) {
+				System.out.println("Esta arma utiliza: " + this.Gun_list.get(option-1).getEnergyAmmoCost() + " celda de energia");
+					}
+					else if(this.Gun_list.get(option-1).getEnergyAmmoCost()>1) {
+						System.out.println("Esta arma utiliza: " + this.Gun_list.get(option-1).getEnergyAmmoCost() + " celdas de energia");
+					}
+		}
+		else if(this.Gun_list.get(option-1).getRadAmmoCost()!=0){
+			if(this.Gun_list.get(option-1).getRadAmmoCost()==1) {
+				System.out.println("Esta arma utiliza: " + this.Gun_list.get(option-1).getRadAmmoCost() + " Cartuchos Gamma");
+					}
+					else if(this.Gun_list.get(option-1).getRadAmmoCost()>1) {
+						System.out.println("Esta arma utiliza: " + this.Gun_list.get(option-1).getRadAmmoCost() + " Cartuchos Gamma");
+					}
+		}
+		System.out.println("");
+		System.out.println("Peso: " + this.Gun_list.get(option-1).getWeigh());
 		System.out.println("");
 	}
 	
-	public void showArmor(Armor A) {
-
+	public void showAllArmor() {
+		for(int i=0;i<this.Armor_list.size();i++) {
 		System.out.println("----------------------------");
 		System.out.println("");
-		System.out.println("Tienes un: " + A.getName());
+		System.out.println("  " + this.Armor_list.get(i).getName());
 		System.out.println("");
 		System.out.println("Resistencia:");
-		System.out.println("  Fisico: "+A.getFisicDefend());
-		System.out.println("  Energia: "+A.getEnergyDefend());
-		System.out.println("  Radiacion: "+A.getRadDefend());
+		System.out.println("  Fisico: "+this.Armor_list.get(i).getFisicDefend());
+		System.out.println("  Energia: "+this.Armor_list.get(i).getEnergyDefend());
+		System.out.println("  Radiacion: "+this.Armor_list.get(i).getRadDefend());
+		System.out.println("");
+		System.out.println("Peso: "+this.Armor_list.get(i).getWeigh());
+	}
+}
+	
+	public void showArmorNames() {
+		for(int i=0;i<this.Armor_list.size();i++) {
+			System.out.println(i+1+"-" + this.Armor_list.get(i).getName());
+		}
+	}
+
+	public void showArmor(int option) {
+		System.out.println("----------------------------");
+		System.out.println("");
+		System.out.println("  " + this.Armor_list.get(option-1).getName());
+		System.out.println("");
+		System.out.println("Resistencia:");
+		System.out.println("  Fisico: "+this.Armor_list.get(option-1).getFisicDefend());
+		System.out.println("  Energia: "+this.Armor_list.get(option-1).getEnergyDefend());
+		System.out.println("  Radiacion: "+this.Armor_list.get(option-1).getRadDefend());
+		System.out.println("");
+		System.out.println("Peso: "+this.Armor_list.get(option-1).getWeigh());
+	}
+	
+		public int weigh() {
+		int TotalWeigh = 0;
+		for(int i=0;i<this.Gun_list.size();i++) {
+			TotalWeigh =TotalWeigh + this.Gun_list.get(i).getWeigh();
+			}
+		for(int i=0;i<this.Armor_list.size();i++) {
+			TotalWeigh=TotalWeigh + this.Armor_list.get(i).getWeigh();
+			}
+		return  TotalWeigh;
 	}
 	
 	public double applyRad(double Max_H,double Rad) {
--- Zork-Text_Game/src/Main.java
+++ Zork-Text_Game/src/Main.java
@@ -13,10 +13,17 @@ public class Main {
 		Scanner a=new Scanner(System.in);
 		
 		Human Humano = new Human();
+		Humano.Gun_list.add(Arma);
+		Humano.Gun_list.add(Arma1);
+		Humano.Gun_list.add(Arma2);
+		Humano.Gun_list.add(Arma3);
+		Humano.Armor_list.add(Armadura);
+		Humano.Armor_list.add(Armadura1);
+		Humano.Armor_list.add(Armadura2);
 		for(int i=0;i<=6;i++) {
 			Humano.SPECIAL[i]=0;
 		}
-		System.out.println("Es hora de saber que te hace SPECIAL, tienes 10 puntos de habilidad para tus atributos");
+		System.out.println("Es hora de saber que te hace S.P.E.C.I.A.L");
 		System.out.println("");
 		int Lvs=10;
 		do {
@@ -26,19 +33,19 @@ public class Main {
 			 System.out.println("");
 		    System.out.println("Cada Atributo tiene un maximo de 10 puntos");
 		    System.out.println("");
-			System.out.println("   1- FUERZA (" + Humano.SPECIAL[0] +"): La FUERZA hace que puedas llevar mas cosas contigo (+10 al Peso Maximo por nivel) \n   y causas mas daño con tus puños ( +2 al daño de los puños por nivel)  ");
+			System.out.println("   1- FUERZA (" + Humano.SPECIAL[0] +"): La FUERZA hace que puedas llevar mas cosas contigo (+10 al Peso Maximo por nivel) \n   y causas mas daño con tus puños ( +2 al daño de los puños por nivel)");
 			System.out.println("");
 			System.out.println("   2- PERCEPCION (" + Humano.SPECIAL[1] +"): Mientras mas PERCEPCION tengas mas facil es encontrar objetos en el yermo(+0.05% de encontrar objetos por nivel)");
 			System.out.println("");
 			System.out.println("   3- RESISTENCIA (" + Humano.SPECIAL[2] +"): La RESISTENCIA aumenta la salud de tu personaje(+10 de salud maxima por nivel)");
 			System.out.println("");
-			System.out.println("   4- CARISMA (" + Humano.SPECIAL[3] +"): La CARISMA hace que tu personaje pueda dialogar con los habitantes del yermo y \n   domesticar a bestias para que te dejen en paz (+0.05% de exito al hablar por nivel ");
+			System.out.println("   4- CARISMA (" + Humano.SPECIAL[3] +"): La CARISMA hace que tu personaje pueda dialogar con los habitantes del yermo y \n   domesticar a bestias para que te dejen en paz (+0.05% de exito al hablar por nivel)");
 			System.out.println("");
 			System.out.println("   5- INTELIGENCIA (" + Humano.SPECIAL[4] +"):La INTELIGENCIA hace que consigas mas experiencia al vencer a un enemigo(+2 de experiencia por enemigo por nivel)");
 			System.out.println("");
-			System.out.println("   6- AGILIDAD (" + Humano.SPECIAL[5] +"): La AGILIDAD aumenta el daño que causas con armas -menos los puños- (+0.1% de daño con armas por nivel");
+			System.out.println("   6- AGILIDAD (" + Humano.SPECIAL[5] +"): La AGILIDAD aumenta el daño que causas con armas -menos los puños- (+0.1% de daño con armas por nivel)");
 			System.out.println("");
-			System.out.println("   7- SUERTE (" + Humano.SPECIAL[6] +"): La SUERTE aumenta la probabildad de que tu proximo ataque sea \n   golpe critico (+0.1% de probabilidad de golpe critico por nivel");
+			System.out.println("   7- SUERTE (" + Humano.SPECIAL[6] +"): La SUERTE aumenta la probabildad de que tu proximo ataque sea \n   golpe critico (+0.1% de probabilidad de golpe critico por nivel)");
 			System.out.println("");
 			System.out.println("Que atributo desea:");
 			int option=a.nextInt();
@@ -182,26 +189,89 @@ public class Main {
 				break;
 			}
 		}while(Lvs >0);
-		
-		
-		Humano.Max_Health=100;
-		Humano.Health=100;
+		Humano.Max_Weigh=50+ (10*Humano.SPECIAL[0]);
+		Humano.Max_Health=100 + (10*Humano.SPECIAL[2]);
+		Humano.Health=100 + (10*Humano.SPECIAL[2]);
 		Humano.Rad=20;
-		System.out.println("Vida: " + Humano.applyRad(Humano.Max_Health,Humano.Rad));
-		
-		Humano.Gun_list.add(Arma);
-		Humano.Gun_list.add(Arma1);
-		Humano.Gun_list.add(Arma2);
-		Humano.Gun_list.add(Arma3);
-		Humano.Armor_list.add(Armadura);
-		Humano.Armor_list.add(Armadura1);
-		Humano.Armor_list.add(Armadura2);
-		
-		for(int i=0;i<Humano.Gun_list.size();i++) {
-		Humano.showGun(Humano.Gun_list.get(i));
-		}
-		for(int i=0;i<Humano.Armor_list.size();i++) {
-			Humano.showArmor(Humano.Armor_list.get(i));
+		int option;
+		int ciclo=1;
+		do {
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println("Salud:" + (int)Humano.applyRad(Humano.Max_Health,Humano.Rad)+"/"+(int)Humano.Max_Health+"      Radiacion:"+(int)Humano.Rad+"      Peso:"+Humano.weigh()+"/"+Humano.Max_Weigh+"      S.P.E.C.I.A.L:("+Humano.SPECIAL[0]+","+Humano.SPECIAL[1]+","+Humano.SPECIAL[2]+","+Humano.SPECIAL[3]+","+Humano.SPECIAL[4]+","+Humano.SPECIAL[5]+","+Humano.SPECIAL[6]+")");
+		System.out.println("");
+		System.out.println("1.Inventario");
+		System.out.println("");
+		System.out.println("Moverse:");
+		System.out.println("  2.Arriba");
+		System.out.println("  3.Abajo");
+		System.out.println("  4.Derecha");
+		System.out.println("  5.Izquierda");
+		option=a.nextInt();
+		switch(option){
+		case 1:
+			System.out.println("------------------------------------");
+			System.out.println("  0.Volver");
+			System.out.println("  1.Armas");
+			System.out.println("  2.Armaduras");
+			System.out.println("  3.Consumibles");
+			System.out.println("");
+			System.out.println("Eleccion:");
+			option=a.nextInt();
+			switch(option) {
+			case 0:
+				ciclo=1;
+				break;
+				
+			case 1:
+				System.out.println("------------------------------------");
+				System.out.println("-1-Ver los detalles de todas las armas");
+				System.out.println("0-Volver");
+						Humano.showGunNames();
+						System.out.println("");
+						System.out.println("Elige una arma para ver su detalle:");
+						option=a.nextInt();
+				if(option==0) {
+					ciclo=1;
+				}
+					else if(option==-1) {
+					Humano.showAllGun();
+				}
+				else{
+					if(option>Humano.Gun_list.size()) {
+						System.out.println("No tienes una arma numero: "+option);
+					}
+					else {
+						Humano.showGun(option);
+					}
+				}
+				break;
+				
+			case 2:
+				System.out.println("------------------------------------");
+				System.out.println("-1-Ver los detalles de todas las armaduras");
+				System.out.println("0-Volver");
+						Humano.showArmorNames();
+						System.out.println("");
+						System.out.println("Elige una armadura para ver su detalle:");
+						option=a.nextInt();
+					if(option==0) {
+						ciclo=1;
+					}
+				else if(option==-1) {
+					Humano.showAllArmor();
+				}
+				else{
+					if(option>Humano.Armor_list.size()) {
+						System.out.println("No tienes una armadura numero: "+option);
+					}
+					else {
+						Humano.showArmor(option);
+					}
+				}
+				break;
+				
+				}
 			}
-}
+		}while(ciclo==1);	
+	}
 }
